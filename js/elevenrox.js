96da2acdee13a9c7434ab818b3648f45d9fcad1f
= <=>
2013-10-13 13:34:51 +0100

    handling generic requests, debugging fails

--- js/elevenrox.js
+++ js/elevenrox.js
@@ -45,7 +45,8 @@ ElevenRox.prototype.upload = function () {
 
 ElevenRox.prototype._login = function () {
 
-	var request = {};
+	var request = {},
+	    obj = this;
 
 	request.method = "login";
 	request.params = {};
@@ -54,19 +55,58 @@ ElevenRox.prototype._login = function () {
 
 	request.id = 1;
 
-	this._send(request);
+	this._send(request,function(_resp) { obj._login_cb(_resp) });
 
 };
 
-ElevenRox.prototype._handle = function (_resp) {
+ElevenRox.prototype._login_cb = function (_resp) {
+
+	var fn = 'ElevenRox._login_cb: ';
+
+	if (!this._resp_landing(_resp)){
+
+		console.log(fn + 'login failed!');
+		return;
+	}
+
+	console.log(fn + 'login successful!');
+};
+
+/*
+ * Should be invoked from callback overrides, will set standard globals up (timesheet token etc)
+ *
+ * Returns true if request was succesfull (in elevenrox), else false
+ */
+ElevenRox.prototype._resp_landing = function (_resp) {
+
+	var fn = 'ElevenRox._resp_landing: ';
 
 	console.log(_resp);
+
+	if (_resp.error) {
+
+		err_string = fn + 'request failed: '
+
+		for (var key in _resp.error) {
+			err_string += key + '- ' + eval('_resp.error.' + key) + ' ';
+		}
+
+		console.log(err_string);
+
+		return false;
+	} else {
+		// all successful responses from 11rx should return a token - need to keep it updated
+		this.token = _resp.result.token;
+		return true;
+	}
 };
 
-ElevenRox.prototype._send = function (_req) {
-
-	var obj = this;
+/*
+ * _req      - request object to send to elevenrox
+ * _callback - callback function which should be exectued after the standard handler
+ */
+ElevenRox.prototype._send = function (_req, _callback) {
 
 	// make the request
-	jQuery.post(this.url, JSON.stringify(_req), obj._handle, "json");
+	jQuery.post(this.url, JSON.stringify(_req), _callback, "json");
 };
